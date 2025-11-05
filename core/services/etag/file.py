6194d82e6b15596ff89008c182cbee3768396a16
Scott Guertin <sguertin@gmail.com>
2025-11-05 12:09:22 -0500

    Moved and renamed files

--- core/services/etag/file.py
+++ core/services/etag/file.py
@@ -9,29 +9,29 @@ from core.interfaces.e_tag import IETagService
 
 class ETagJsonFileService(IETagService):
     file_path: Path
-    _e_tags: dict[str, str]
+    _etags: dict[str, str]
     last_modified: datetime | None = None
 
     @property
-    def e_tags(self) -> dict[str, str]:
-        self.reload_e_tags()
-        return self._e_tags
+    def etags(self) -> dict[str, str]:
+        self.reload_etags()
+        return self._etags
 
     def __init__(self, settings: FileSettings):
-        self.file_path = Path(settings.e_tag_file_path)
-        self.reload_e_tags()
+        self.file_path = Path(settings.etag_file_path)
+        self.reload_etags()
 
-    def get_e_tag(self, xml_url: str) -> str | None:
-        return self.e_tags.get(xml_url)
+    def get_etag(self, xml_url: str) -> str | None:
+        return self.etags.get(xml_url)
 
-    def set_e_tag(self, xml_url: str, e_tag: str) -> None:
-        self.reload_e_tags()
-        self._e_tags[xml_url] = e_tag
-        json.write_json_file(self.file_path, self._e_tags)
+    def set_etag(self, xml_url: str, etag: str) -> None:
+        self.reload_etags()
+        self._etags[xml_url] = etag
+        json.write_json_file(self.file_path, self._etags)
         self.last_modified = datetime.now()
 
-    def reload_e_tags(self) -> None:
+    def reload_etags(self) -> None:
         last_modified = file.file_modification_date(self.file_path)
         if self.last_modified is None or last_modified > self.last_modified:
             self.last_modified = last_modified
-            self._e_tags = json.read_json_file(self.file_path)
+            self._etags = json.read_json_file(self.file_path)
