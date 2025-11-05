cf7831e34eb0e355b74df6d0c439a51279608eaa
Scott Guertin <sguertin@gmail.com>
2025-11-05 12:20:25 -0500

    Adds IRssReaderService and IRssStorageService interfaces

--- core/interfaces/rss.py
+++ core/interfaces/rss.py
@@ -39,3 +39,33 @@ class IRssReaderService(ABC, metaclass=ABCMeta):
             FeedItem:
         """
         pass
+
+
+class IRssStorageService(ABC, metaclass=ABCMeta):
+
+    @abstractmethod
+    def get_stored_items(self) -> list[FeedItem]:
+        """Retrieves all feed items currently in storage
+
+        Returns:
+            list[FeedItem]: the stored feed items
+        """
+        pass
+
+    @abstractmethod
+    def store_feed_item(self, item: FeedItem) -> None:
+        """Add a feed item to storage
+
+        Args:
+            item (FeedItem): the feed item to be stored
+        """
+        pass
+
+    @abstractmethod
+    def store_feed_items(self, items: list[FeedItem]) -> None:
+        """Add multiple feed items to storage
+
+        Args:
+            items (list[FeedItem]): the feed items to be stored
+        """
+        pass
